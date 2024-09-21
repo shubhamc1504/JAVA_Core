05e5d0a2fcd7e7b30bb0517aa993c8229e9f91f1
shubhamc1504 <shubhamc1504@gmail.com>
2024-09-21 17:02:13 +0530

    Initial Commit

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String args[])
+    {
+        int n = 5;
+        int result= n % 2 ==0 ? 10 : 20;
+        System.out.println(result);
+    }
+}
