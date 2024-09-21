55766f431a2060190794774d2e67bfc9f8331ef6
shubhamc1504 <shubhamc1504@gmail.com>
2024-09-21 10:01:11 +0530

    Initial Commit

---
+++ PrimitiveDataType.java
@@ -0,0 +1,23 @@
+// Primitive Data Types-> Integer, Float, Character, Boolean
+//Integer(byte[1byte/8bits],short[2bytes/16bits],int[4bytes/32bits],long[8bytes/64bits]).Eg: int i=5
+//Float(double[8bytes/64bits],float[4bytes/32bits]). Eg: double d = 5.6,float f = 5.6f
+//Character. Eg: Char c ='k';
+//Boolean(true,false). Eg: boolean b = true
+
+class Hello
+{
+    int num = 9;
+    byte b = 127;
+    short s = 575;
+    long l = 54567;
+    
+    char c ='s';
+
+    float f =5.8f;
+    double d = 5.8;
+
+    boolean bo =true;
+
+}
+
+
