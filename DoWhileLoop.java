3f70598fc1b3b62102f3399c4032dccc2918969a
shubhamc1504 <shubhamc1504@gmail.com>
2024-09-21 20:20:11 +0530

    Initial Commit

---
+++ DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main(String[] args)
+    {
+        
+        int i = 1;
+        do{
+            System.out.println("Hi " + i);
+            i++;
+        }while (i <= 5);
+    }
+}
+
