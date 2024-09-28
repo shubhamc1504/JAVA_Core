2d1a2573d9d074b12dd429a4532199cc18b6a743
shubhamc1504 <shubhamc1504@gmail.com>
2024-09-28 18:35:07 +0530

    Initial Commit

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,26 @@
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        int nums[][]= new int[3][4]; //nums var with 
+        for(int i=0; i<3;i++) //rows
+        {
+            for(int j=0;j<4;j++) //columns
+            {
+                nums[i][j]= (int)(Math.random()*10); //type casted random numbers to int 
+                System.out.println(nums[i][j]);
+            }
+            System.out.println();
+        }
+
+        // for(int n[]:nums) //For each loop
+        // {
+
+        //     for(int m:n)
+        //     {
+        //         System.out.println(m);
+                
+        //     }
+        // }
+
+    }
+    
+}
